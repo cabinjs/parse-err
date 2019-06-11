07c2962252abd4cb21f053bb29339848fa3a2f81
niftylettuce <niftylettuce@gmail.com>
2019-06-11 14:03:19 -0500

    feat: removed lodash and reduced package size drastically, added test

--- test/test.js
+++ test/test.js
@@ -1,3 +1,19 @@
 const test = require('ava');
 
-test.todo('write tests');
+const parseErr = require('..');
+
+class CustomError extends Error {
+  constructor(...params) {
+    super(params);
+    Error.captureStackTrace(this, CustomError);
+    Object.defineProperty(this, 'foo', {
+      value: 'beep',
+      enumerable: false
+    });
+  }
+}
+
+test('basic', t => {
+  const customError = new CustomError();
+  t.is(parseErr(customError).foo, 'beep');
+});
